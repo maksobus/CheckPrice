d01ad66e6da15f87ea66ada32b93f24e2b2693fc
WOT <phpgraber@gmail.com>
2023-11-20 14:26:56 +0200

    beta

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,22 @@
-
-
 chrome.tabs.onUpdated.addListener(checkTab);
 
-
 function checkTab() {
-  chrome.tabs.query({active: true}, function(tabs) {
-    var tab = tabs[0];
-    if (tab.status != 'complete') return;
-    if (!tab.url) return;
+    chrome.tabs.query({active: true}, function (tabs) {
+        let tab = tabs[0];
+        if (tab.status !== 'complete') return;
+        if (!tab.url) return;
 
-    console.log(tab.status);
-    var regex = /item\/\d+\.html/;
+        console.log(tab.status);
+        let regex = /item\/\d+\.html/;
 
-    if (regex.test(tab.url)) {
-      chrome.scripting
-          .executeScript({
-            target : {"tabId" : tab.id},
-            files : [ "script.js" ],
-          })
-          .then(() => console.log("script injected"));
-    }
-
-  });
+        if (regex.test(tab.url)) {
+            chrome.scripting
+                .executeScript({
+                    target: {"tabId": tab.id},
+                    files: ["script.js"],
+                })
+                .then(() => console.log("script injected"));
+        }
+    });
 }
 
